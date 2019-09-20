f127755f7473b5e70bc6a9c12d0248189b988cfd
Christian Gregorio <cgregorio026@west-mec.org>
2019-09-19 17:26:09 -0700

    Zip Code
    Used the ability to select different countries in the document.

--- Projects/Project02/script.js
+++ Projects/Project02/script.js
@@ -9,18 +9,24 @@
 "use strict";
 
 var httpRequest = false;
+var countrySel;
 // defines process to make a request
 function getRequestObject() {
     try {
         httpRequest = new XMLHttpRequest();
-    }
-    catch (requestError) {
+    } catch (requestError) {
+        document.getElementById("zipset").style.visibility = "visible";
         document.getElementById("csset").style.visibility = "visible";
+        var germany = document.getElementById("germany");
+        var us = document.getElementById("us");
         var zip = document.getElementById("zip").value;
         if (zip.addEventListener) {
+            germany.removeEventListener("click", checkButtons, false);
+            us.removeEventListener("click", checkButtons, false);
             zip.removeEventListener("keyup", checkInput, false);
-        } 
-        else if (zip.attachEvent) {
+        } else if (zip.attachEvent) {
+            germany.detachEvent("onclick", checkButtons);
+            us.detachEvent("onclick", checkButtons);
             zip.detachEvent("onkeyup", checkInput);
         }
         return false;
@@ -29,21 +35,34 @@ function getRequestObject() {
     return httpRequest;
 }
 
-function checkInput(){
+function checkButtons() {
+    // alert("checkButtons()");
+    var germany = document.getElementById("germany");
+    var us = document.getElementById("us");
+    if (germany.checked || us.checked) {
+        document.getElementById("zipset").style.visibility = "visible";
+        if (germany.checked) {
+            countrySel = "de";
+        } else {
+            countrySel = "us";
+        }
+    }
+}
+
+function checkInput() {
     // alert("checkInput()");
     var zip = document.getElementById("zip").value;
     // checks to see if the zip code is exactly 5 letters.
     if (zip.length === 5) {
         // calls the getLocation function ONLY when a full zip code is detected.
-        getLocation()
-    }
-    else {
+        getLocation();
+    } else {
         document.getElementById("city").value = "";
         document.getElementById("state").value = "";
     }
 }
 
-function getLocation(){
+function getLocation() {
     // console.log("getLocation()");
     var zip = document.getElementById("zip").value;
     // performs a get request to the server to obtain data from an array.
@@ -51,11 +70,7 @@ function getLocation(){
         httpRequest = getRequestObject();
     }
     // gets the data from the api listed in the http request section
-    httpRequest.abort();
-    httpRequest.open("get", "http://api.zippopotam.us/us/" + zip, true);
-    httpRequest.send(null);
-    httpRequest.onreadystatechange = displayData;
-
+ 
     function displayData() {
         if (httpRequest.readyState === 4 && httpRequest.status === 200) {
             var resultData = JSON.parse(httpRequest.responseText);
@@ -68,11 +83,27 @@ function getLocation(){
             document.getElementById("csset").style.visibility = "visible";
         }
     }
+
+    var germany = document.getElementById("germany");
+    var us = document.getElementById("us");
+    if (us.addEventListener) {
+        germany.addEventListener("click", checkButtons, false);
+        us.addEventListener("click", checkButtons, false);
+    } else if (us.attachEvent) {
+        germany.attachEvent("onclick", checkButtons);
+        us.attachEvent("onclick", checkButtons);
+    }
+
+    httpRequest.abort();
+    httpRequest.open("get", "http://api.zippopotam.us/" + countrySel + "/" + zip, true);
+    httpRequest.send(null);
+    httpRequest.onreadystatechange = displayData;
+
 }
 // when a user releases a key, we will check the CheckInput command
 var zip = document.getElementById("zip");
-if (zip.addEventListener){
+if (zip.addEventListener) {
     zip.addEventListener("keyup", checkInput, false);
-} else if (zip.attachEvent){
+} else if (zip.attachEvent) {
     zip.attachEvent("onkeyup", checkInput);
 }
